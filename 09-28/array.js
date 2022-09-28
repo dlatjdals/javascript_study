cf15adc292136a1e636f7531afc819cbe0954776
dlatjdals <simon050204@naver.com>
2022-09-28 12:07:51 +0900

    qwe

---
+++ 09-28/array.js
@@ -0,0 +1,9 @@
+let students = ["문지영", "신현성", "임성민", "류한서"];
+students.push("강유림");
+students.unshift("임다영");
+students.unshift("고근국쌤");
+students.pop();
+students.shift();
+students.splice(2, 1, "근국쌤");
+console.log(students); 
+
---
+++ 09-28/funtion.js
@@ -0,0 +1,10 @@
+function plus(num1 , num2) {
+    return num1 + num2;
+    //console.log("Plus:", num1 + num2);
+}
+function minus(num1, num2) {
+    return num1 - num2;
+}
+let b = minus(1, 2);
+let a = plus(3, 4) - b;
+console.log(a);

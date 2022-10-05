e86912f8788e3efd9a3330ca332cf11efdf1959e
dlatjdals <simon050204@naver.com>
2022-10-05 12:05:49 +0900

    내장함수 , jquery

---
+++ 10.05/index.js
@@ -0,0 +1,13 @@
+let kotoday = new Date().toLocaleString('ko-KR',{
+    timeZone: 'Aisa/Seoul'
+});
+
+let today = new Date();
+
+console.log(today.getFullYear());
+console.log(today.getMonth());
+console.log(today.getDate)
+console.log(today.getDay());
+console.log(today.getHours());
+console.log(today.getMinutes());
+console.log(today.getSecons());
